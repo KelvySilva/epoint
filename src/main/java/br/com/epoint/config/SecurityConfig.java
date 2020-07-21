ae56597cd09f24b5a063b0784931846c9d1c74ae
SGCloud <kylmax@hotmail.com>
2020-07-21 11:14:50 -0300

    Removendo comments

--- src/main/java/br/com/epoint/config/SecurityConfig.java
+++ src/main/java/br/com/epoint/config/SecurityConfig.java
@@ -41,40 +41,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .disable();
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.cors()
-//                .configurationSource(req -> new CorsConfiguration().applyPermitDefaultValues())
-//                .and()
-//                .csrf()
-//                .disable()
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.GET,SIGN_UP_URL).permitAll()
-//                .antMatchers("/*/protected/**").hasRole("USER")
-//                .antMatchers("/*/admin/**").hasRole("ADMIN")
-//                .and()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
-//                .addFilter(new JWTAuthorizationFilter(authenticationManager(),customUserDetailService));
-//
-//    }
-//
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-//    /**
-//     * Método usado para autenticação em memória.
-//     * @param builder
-//     * @throws Exception
-//     */
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
-//        builder.inMemoryAuthentication()
-//                .withUser("spring").password("{noop}spring").roles("USER")
-//        .and()
-//                .withUser("admin").password("{noop}aj[lo12po").roles("USER","ADMIN");
-//    }
+
 }
